d538a9a7e9b0206ccbe56ef9192224e7c7d5a6ca
Ur-imazing <urim027@gmail.com>
2025-10-16 16:54:05 +1300

    chore: format with prettier

--- src/ios/runner.ts
+++ src/ios/runner.ts
@@ -39,7 +39,7 @@ export async function runner({
     [transformCountries, "countries"],
     [transformMediaCategories, "mediaCategories"],
     [transformMediaItems, "mediaItems"],
-    [transformContainedByMediaLinks, "containedByMediaLinks"]
+    [transformContainedByMediaLinks, "containedByMediaLinks"],
   ]
 
   // Add reading language data transformer if requested
