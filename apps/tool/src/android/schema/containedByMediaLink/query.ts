33135f4cc745d67538fe69c95b137e8d1f2dfcdb
Ur-imazing <urim027@gmail.com>
2025-11-12 14:24:14 +1300

    fix: add fetch limit to ContainedByMediaLinkQuery to fetch correct number of video objects

--- apps/tool/src/android/schema/containedByMediaLink/query.ts
+++ apps/tool/src/android/schema/containedByMediaLink/query.ts
@@ -2,7 +2,10 @@ import { graphql } from "gql.tada"
 
 export const JFPAppCacheBuilder_iOS_ContainedByMediaLinkQuery = graphql(`
   query JFPAppCacheBuilder_iOS_ContainedByMediaLinkQuery {
-    videos(where: { labels: [collection, series] }) {
+    videos(
+      where: { labels: [collection, series, featureFilm, shortFilm] }
+      limit: 1000
+    ) {
       parentMediaComponentId: id
       children {
         mediaComponentId: id
