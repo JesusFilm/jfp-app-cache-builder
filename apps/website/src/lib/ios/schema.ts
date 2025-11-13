5e78723e164b09215b41d7d34a6fc9b4177bd664
Ur-imazing <urim027@gmail.com>
2025-11-13 14:27:40 +1300

    chore: ignore eslint error for using ts-ignore

--- apps/website/src/lib/ios/schema.ts
+++ apps/website/src/lib/ios/schema.ts
@@ -3,7 +3,8 @@ import { type TableInfo, tableInfoSchema } from "../common/schema"
 let tables: TableInfo[]
 
 try {
-  // eslint-disable-next-line
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore - schema.json is generated at build time
   const iosSchema = await import("./data/schema.json")
   const validatedSchema = tableInfoSchema.parse(iosSchema.default)
   tables = validatedSchema
