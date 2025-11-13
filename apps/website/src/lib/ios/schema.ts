7ed014ac6019d9697f68f19403635635b34ccccd
Ur-imazing <urim027@gmail.com>
2025-11-13 14:35:58 +1300

    chore: use disable-enable comment block to apply eslint ignore directive for multiple lines of code

--- apps/website/src/lib/ios/schema.ts
+++ apps/website/src/lib/ios/schema.ts
@@ -3,9 +3,10 @@ import { type TableInfo, tableInfoSchema } from "../common/schema"
 let tables: TableInfo[]
 
 try {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  /* eslint-disable @typescript-eslint/ban-ts-comment */
   // @ts-ignore - schema.json is generated at build time
   const iosSchema = await import("./data/schema.json")
+  /* eslint-enable @typescript-eslint/ban-ts-comment */
   const validatedSchema = tableInfoSchema.parse(iosSchema.default)
   tables = validatedSchema
 } catch {
